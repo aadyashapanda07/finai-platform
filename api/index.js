b8c9a0aa1ea436813b9755fcd43f33741e5fa8c7
Aadyasha panda <aadyashapanda@example.com>
2026-09-24 19:54:05 +0530

    Diagnose Vercel API error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,31 @@
-const app = require('../server/src/index');
+let app;
+let initError = null;
 
-module.exports = app;
+try {
+  app = require('../server/src/index');
+} catch (err) {
+  initError = {
+    message: err.message,
+    stack: err.stack
+  };
+}
+
+module.exports = (req, res) => {
+  if (initError) {
+    return res.status(200).json({
+      success: false,
+      error: 'INIT_ERROR',
+      details: initError
+    });
+  }
+  try {
+    return app(req, res);
+  } catch (err) {
+    return res.status(200).json({
+      success: false,
+      error: 'RUNTIME_ERROR',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
